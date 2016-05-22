5505c98d99a614cd27c126d14011227b88608391
giria <joan@giria.com>
2016-05-22 20:25:21 +0200

    first commit

---
+++ Networking1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networking1
+//
+//  Created by Joan Barrull Ribalta on 22/05/16.
+//  Copyright © 2016 com.giria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
